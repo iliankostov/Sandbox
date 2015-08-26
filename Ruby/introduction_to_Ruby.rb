07e6e52604c89f40fa3fcb5681a5b68a360d3ad3
iliankostov <ilian.kostov@gmail.com>
2015-08-26 23:07:51 +0300

    Solve Ruby introduction course up to 46%

---
+++ Ruby/introduction_to_Ruby.rb
@@ -0,0 +1,27 @@
+my_num = 25
+
+my_boolean = true
+
+my_string = "Ruby"
+
+my_num = 100
+
+exp = 2 ** 3 # exponentional
+
+mod = 25 % 7 # modular
+
+puts "Hello " + my_string + " !" # hello
+=begin
+comment
+=end
+
+my_name = "Ilian"
+my_age = 31
+
+sum = 13 + 379
+product = 923 * 15
+quotient = 13209 / 17
+
+name = "Ilian"
+name.downcase.reverse.upcase # chaining
+
