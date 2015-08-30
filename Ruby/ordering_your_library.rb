7bf323c4e47b797264059abbf7d28f45de67eea6
iliankostov <ilian.kostov@gmail.com>
2015-08-30 15:57:14 +0300

    Create own ordering function

---
+++ Ruby/ordering_your_library.rb
@@ -0,0 +1,18 @@
+def alphabetize(arr, reverse = false)
+  if reverse
+    arr.sort {|item1, item2| item2 <=> item1}
+  else
+    arr.sort {|item1, item2| item1 <=> item2}
+  end
+end
+
+books = [
+    "Heart of Darkness",
+    "Code Complete",
+    "The Lorax",
+    "The Prophet",
+    "Absalom, Absalom"
+]
+
+puts "A-Z: #{alphabetize(books)}"
+puts "Z-A: #{alphabetize(books, true)}"
