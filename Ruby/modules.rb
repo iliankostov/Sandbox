ab0bef49d5840e0a4ea9a972db49882a1852612d
iliankostov <ilian.kostov@gmail.com>
2015-09-01 10:00:31 +0300

    Exercise object-oriented programming

---
+++ Ruby/modules.rb
@@ -0,0 +1,115 @@
+def delimiter
+  20.times { print '-' }
+  puts
+end
+
+module Circle
+
+  PI = Math::PI
+
+  def Circle.area(radius)
+    PI * radius**2
+  end
+
+  def Circle.circumference(radius)
+    2 * PI * radius
+  end
+end
+
+puts Circle.area(10)
+puts Circle.circumference(10)
+
+delimiter
+
+puts Math::PI
+
+delimiter
+
+require 'date'
+puts Date.today
+
+delimiter
+
+class Angle
+  include Math
+  attr_accessor :radians
+
+  def initialize(radians)
+    @radians = radians
+  end
+
+  def cosine
+    cos(@radians)
+  end
+end
+
+acute = Angle.new(1)
+puts acute.cosine
+
+delimiter
+
+module Action
+  def jump
+    @distance = rand(4) + 2
+    puts "I jumped forward #{@distance} feet!"
+  end
+end
+
+class Rabbit
+  include Action
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Cricket
+  include Action
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+peter = Rabbit.new("Peter")
+jiminy = Cricket.new("Jiminy")
+
+peter.jump
+jiminy.jump
+
+delimiter
+
+module MartialArts
+  def swordsman
+    puts "I'm a swordsman."
+  end
+end
+
+class Ninja
+  include MartialArts
+  def initialize(clan)
+    @clan = clan
+  end
+end
+
+class Samurai
+  include MartialArts
+  def initialize(shogun)
+    @shogun = shogun
+  end
+end
+
+delimiter
+
+module ThePresent
+  def now
+    puts "It's #{Time.new.hour > 12 ? Time.new.hour - 12 : Time.new.hour}:#{Time.new.min} #{Time.new.hour > 12 ? 'PM' : 'AM'} (GMT)."
+  end
+end
+
+class TheHereAnd
+  extend ThePresent
+end
+
+TheHereAnd.now
+
