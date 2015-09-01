7d79e28a948759c9f4a85e5a62a568ca5e609858
iliankostov <ilian.kostov@gmail.com>
2015-09-01 09:07:38 +0300

    Exercise object-oriented programming

---
+++ Ruby/object-oriented_programming.rb
@@ -0,0 +1,103 @@
+def delimiter
+  20.times { print '-' }
+  puts
+end
+
+class Language
+  def initialize(name, creator)
+    @name = name
+    @creator = creator
+  end
+
+  def description
+    puts "I'm #{@name} and I was created by #{@creator}!"
+  end
+end
+
+ruby = Language.new("Ruby", "Yukihiro Matsumoto")
+python = Language.new("Python", "Guido van Rossum")
+javascript = Language.new("JavaScript", "Brendan Eich")
+
+ruby.description
+python.description
+javascript.description
+
+delimiter
+
+class Computer
+  $manufacturer = "Mango Computer, Inc."
+  @@files = {hello: "Hello, world!"}
+
+  def initialize(username, password)
+    @username = username
+    @password = password
+  end
+
+  def current_user
+    @username
+  end
+
+  def self.display_files
+    @@files
+  end
+end
+
+# Make a new Computer instance:
+hal = Computer.new("Dave", 12345)
+
+puts "Current user: #{hal.current_user}"
+# @username belongs to the hal instance.
+
+puts "Manufacturer: #{$manufacturer}"
+# $manufacturer is global! We can get it directly.
+
+puts "Files: #{Computer.display_files}"
+# @@files belongs to the Computer class.
+
+delimiter
+
+def create_record(attributes, raise_error = false)
+  record = build_record(attributes)
+  yield(record) if block_given?
+  saved = record.save
+  set_new_record(record)
+  raise RecordInvalid.new(record) if !saved && raise_error
+  record
+end
+
+delimiter
+
+class ApplicationError
+  def display_error
+    puts "Error! Error!"
+  end
+end
+
+class SuperBadError < ApplicationError
+end
+
+err = SuperBadError.new
+err.display_error
+
+delimiter
+
+class Creature
+  def initialize(name)
+    @name = name
+  end
+
+  def fight
+    'Punch to the chops!'
+  end
+end
+
+class Dragon < Creature
+  def fight
+    super
+      puts 'Instead of breathing fire...'
+      super
+  end
+end
+
+dragon = Dragon.new("dragon")
+dragon.fight
